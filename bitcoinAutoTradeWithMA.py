f39c40808692e08e8ffc7a6f0546a4d8e65cff95
KUNCHANG LEE <lkc9233@naver.com>
2022-01-23 18:22:39 +0900

    K값 변경

--- bitcoinAutoTradeWithMA.py
+++ bitcoinAutoTradeWithMA.py
@@ -19,7 +19,7 @@ def get_start_time(ticker):
 
 def get_ma15(ticker): # MA버전 추가분
     """15일 이동 평균선 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="day", count=15)
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=3)
     ma15 = df['close'].rolling(15).mean().iloc[-1]
     return ma15
 
@@ -50,7 +50,7 @@ while True:
         end_time = start_time + datetime.timedelta(days=1)
 
         if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-NEAR", 0.5)
+            target_price = get_target_price("KRW-NEAR", 0.4)
             ma15 = get_ma15("KRW-NEAR") # MA버전 삽입분
             current_price = get_current_price("KRW-NEAR")
             benefit_price = target_price * 1.15 # 익절조건은 매수 후 15%상승시
