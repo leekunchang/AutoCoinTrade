504d6ca0de5238516cf2ef53f4e8f574d38e2564
KUNCHANG LEE <lkc9233@naver.com>
2022-01-30 11:50:54 +0900

    commit2

--- bitcoinAutoTradeWithMA.py
+++ bitcoinAutoTradeWithMA.py
@@ -51,7 +51,7 @@ print("autotrade start")
 while True:
     try:
         now = datetime.datetime.now()
-        start_time = get_start_time("KRW-SNAD")
+        start_time = get_start_time("KRW-SAND")
         end_time = start_time + datetime.timedelta(days=1)
 
         if start_time < now < end_time - datetime.timedelta(seconds=10):
